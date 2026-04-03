a65f7e9956bf35c6ca45b3e3b3ca47f89c1cb3ad
sarthak <patilsarthak53038@gmail.com>
2026-04-03 18:32:28 +0530

    Fix config for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    
+    app.config['SECRET_KEY'] = 'zorvyn-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finance.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = 'zorvyn-jwt-secret'
 
     db.init_app(app)
     jwt.init_app(app)
