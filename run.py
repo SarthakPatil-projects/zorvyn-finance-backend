0afe9ecff1dd7ed85284507981dd4dee9bcb9743
sarthak <patilsarthak53038@gmail.com>
2026-04-03 18:21:45 +0530

    Fix deployment config

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ import os
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
